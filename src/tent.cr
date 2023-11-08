16e806f97d5f7282a2d8e61bc53a8a7853a8c2df
satler <119724672+satler-git@users.noreply.github.com>
2023-11-08 00:52:54 +0000

    Lint

--- src/tent.cr
+++ src/tent.cr
@@ -6,4 +6,3 @@ endpoint = Const::ENDPOINT
 now = Time.local Time::Location.load("Asia/Tokyo")
 end_time = Time.local(now.year, now.month, now.day, 13, 5, 0, location: Time::Location.load("Asia/Tokyo"))
 Player.new(endpoint, "./tent.db").play(end_time)
-
