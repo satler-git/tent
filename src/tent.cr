4f667aac0c4a84c4f5b78729ae3a837850eebc86
satler-git <mitsclatw24@mbr.nifty.com>
2023-12-21 07:43:05 +0000

    style: format via default linter

--- src/tent.cr
+++ src/tent.cr
@@ -11,7 +11,6 @@ def endpoint : String
   end
 end
 
-
 endpoint = endpoint()
 now = Time.local Time::Location.load("Asia/Tokyo")
 end_time = Time.local(now.year, now.month, now.day, 13, 5, 0, location: Time::Location.load("Asia/Tokyo"))
