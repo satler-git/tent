03c4c9d377882c2ab9e90074c7e5b35bad370e1e
satler-git <mitsclatw24@mbr.nifty.com>
2024-01-10 11:52:08 +0000

    MPVのyoutube-dlを使うように(chacheが使える)

--- py_api/pplay.py
+++ py_api/pplay.py
@@ -1,7 +1,5 @@
 import sys
-import os
 from ytmusicapi import YTMusic
-import yt_dlp
 
 def search(song_name, artist_name):
     client = YTMusic(language="ja")
@@ -9,29 +7,11 @@ def search(song_name, artist_name):
 
     return search_results[0].get("videoId")
 
-def dl(vid):
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-        'outtmpl': f"tmp/{vid}.%(ext)s",  # 保存先の設定
-        'noprogress': True,
-    }
-    ydl = yt_dlp.YoutubeDL(ydl_opts)
-    video_url = f"https://www.youtube.com/watch?v={vid}"
-    with ydl:
-        result = ydl.extract_info(video_url, download=True)
-    return result
-
-def echo_locate(song_name, artist_name):
+def echo_url(song_name, artist_name):
     vid = search(song_name, artist_name)
-    info_dict = dl(vid)
-    locate = f"{os.getcwd().replace(os.sep,'/')}/tmp/{vid}.mp4"
-    if 'requested_downloads' in info_dict:
-        locate = info_dict['requested_downloads'][0]['filename']
-    else:
-        print("Couldn't find access.")
-    print(locate)
+    print(f"https://www.youtube.com/watch?v={vid}")
 
 if len(sys.argv) == 3:
-    echo_locate(sys.argv[1], sys.argv[2])
+    echo_url(sys.argv[1], sys.argv[2])
 else:
     print("Invalid args count")
--- src/Song.cr
+++ src/Song.cr
@@ -3,14 +3,14 @@ class Song
   property song_name : String
   property artist_name : String
   property count : Int64
-  property locale : String
+  property url : String
 
   def initialize(song_name : String, artist_name : String)
     @id = 0
     @song_name = song_name
     @artist_name = artist_name
     @count = 0
-    @locale = "0"
+    @url = "0"
   end
 
   def initialize(id : Int64, song_name : String, artist_name : String, count : Int64)
@@ -18,31 +18,21 @@ class Song
     @song_name = song_name
     @artist_name = artist_name
     @count = count
-    @locale = "0"
+    @url = "0"
   end
 
   def play(next_song : Song)
-    if (@locale == "0") | File.exists?(locale)
-      # ロケールが設定されていないかファイルがなかったら
-      # ダウンロード
+    if (@url == "0") | File.exists?(locale)
+      # 検索
       process = Process.new("pplay", [@song_name, @artist_name], output: STDOUT)
-      puts "Start downloading #{@song_name} #{@artist_name}"
-      # ダウンロードを待つ
+      puts "Now searching #{@song_name} #{@artist_name}"
+      # 検索を待つ
       process.wait
-      @locale = process.output.gets_to_end
+      @url = process.output.gets_to_end
     end
     # 再生を開始
-    mpv_process = Process.new("mpv", [@locale], output: STDOUT)
+    mpv_process = Process.new("mpv", [@url], output: STDOUT)
     puts "Now playing #{@song_name} #{@artist_name}"
-    # プレダウンロード
-    pre_process = Process.new("pplay", [next_song.song_name, next_song.artist_name], output: STDOUT)
-    puts "Start downloading #{next_song.song_name} #{next_song.song_name}"
-    # ダウンロード完了を待つ
-    pre_process.wait
-    next_song.locale = pre_process.output.gets_to_end
-    # 再生終了を待つ
-    mpv_process.wait
-    next_song
   end
 
   def to_s
